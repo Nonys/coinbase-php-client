bc6f5d5670d85a44b76c0d27289a691b9435daeb
nonys <nonysak62@gmail.com>
2021-02-03 21:32:01 +0100

    first commit

---
+++ src/Client.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Nonys\Coinbase;
+
+/**
+ *
+ * @author Ondrej Novak <nonysak62@gmail.com>
+ */
+class Client
+{
+
+    // Test
+}
---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'nonys/coinbase-php-client',
+  ),
+  'versions' => 
+  array (
+    'nonys/coinbase-php-client' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+  ),
+);
